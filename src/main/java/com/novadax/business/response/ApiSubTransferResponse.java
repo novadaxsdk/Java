79d3211f07b34473f5edcd386c4216277c54b7ee
lijianliang <lijianliang@wecash-mail.com>
2019-12-05 13:21:46 +0800

    sub-account-api

--- src/main/java/com/novadax/business/response/ApiSubTransferResponse.java
+++ src/main/java/com/novadax/business/response/ApiSubTransferResponse.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  */
 public class ApiSubTransferResponse implements Serializable {
     private String subId;
-    private BigDecimal amount;
+    private String amount;
     private String currency;
     private String state;
     private String type;
