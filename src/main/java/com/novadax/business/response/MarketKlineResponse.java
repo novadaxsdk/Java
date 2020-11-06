ea5656aa1c1340d9e2f8979460d00c811e5d93c6
qiujun <qiujun@wecash.net>
2020-11-06 17:49:30 +0800

    rm lombok

--- src/main/java/com/novadax/business/response/MarketKlineResponse.java
+++ src/main/java/com/novadax/business/response/MarketKlineResponse.java
@@ -1,23 +1,13 @@
 package com.novadax.business.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 /**
  * Created by bh on 2020/10/12.
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class MarketKlineResponse implements Serializable{
+public class MarketKlineResponse implements Serializable {
     private static final long serialVersionUID = 764715904809312256L;
-
 
     private String symbol;
 
@@ -36,4 +26,76 @@ public class MarketKlineResponse implements Serializable{
     private BigDecimal highPrice;
 
     private BigDecimal vol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getCount() {
+        return count;
+    }
+
+    public void setCount(BigDecimal count) {
+        this.count = count;
+    }
+
+    public BigDecimal getOpenPrice() {
+        return openPrice;
+    }
+
+    public void setOpenPrice(BigDecimal openPrice) {
+        this.openPrice = openPrice;
+    }
+
+    public BigDecimal getClosePrice() {
+        return closePrice;
+    }
+
+    public void setClosePrice(BigDecimal closePrice) {
+        this.closePrice = closePrice;
+    }
+
+    public BigDecimal getLowPrice() {
+        return lowPrice;
+    }
+
+    public void setLowPrice(BigDecimal lowPrice) {
+        this.lowPrice = lowPrice;
+    }
+
+    public BigDecimal getHighPrice() {
+        return highPrice;
+    }
+
+    public void setHighPrice(BigDecimal highPrice) {
+        this.highPrice = highPrice;
+    }
+
+    public BigDecimal getVol() {
+        return vol;
+    }
+
+    public void setVol(BigDecimal vol) {
+        this.vol = vol;
+    }
 }
